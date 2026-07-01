90fa82a98a5fa511bd2d79e232edebf1d0bd6db7
Cartoffel <jack.schaible@hotmail.com>
2026-06-30 20:25:35 -0600

    fix: Update S3 origin import and adjust deployment source path

--- infra/lib/constructs/frontend.ts
+++ infra/lib/constructs/frontend.ts
@@ -1,16 +1,16 @@
-import * as cdk from 'aws-cdk-lib';
-import { Bucket, BlockPublicAccess } from 'aws-cdk-lib/aws-s3';
-import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import * as cdk from "aws-cdk-lib";
+import { Bucket, BlockPublicAccess } from "aws-cdk-lib/aws-s3";
+import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
 import {
   Distribution,
   ViewerProtocolPolicy,
   AllowedMethods,
   CachePolicy,
   OriginAccessIdentity,
-} from 'aws-cdk-lib/aws-cloudfront';
-import { S3BucketOrigin } from 'aws-cdk-lib/aws-cloudfront-origins';
-import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
-import { Construct } from 'constructs';
+} from "aws-cdk-lib/aws-cloudfront";
+import { S3BucketOrigin } from "aws-cdk-lib/aws-cloudfront-origins";
+import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
+import { Construct } from "constructs";
 
 interface FrontendProps {
   certificate: Certificate;
@@ -22,36 +22,46 @@ export class FringeFrontend extends Construct {
   constructor(scope: Construct, id: string, props: FrontendProps) {
     super(scope, id);
 
-    const bucket = new Bucket(this, 'SiteBucket', {
+    const bucket = new Bucket(this, "SiteBucket", {
       blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
       removalPolicy: cdk.RemovalPolicy.RETAIN,
       autoDeleteObjects: false,
     });
 
-    const oai = new OriginAccessIdentity(this, 'OAI');
+    const oai = new OriginAccessIdentity(this, "OAI");
     bucket.grantRead(oai);
 
-    const distribution = new Distribution(this, 'Distribution', {
+    const distribution = new Distribution(this, "Distribution", {
       defaultBehavior: {
         origin: new S3BucketOrigin(bucket, { originAccessIdentity: oai }),
         viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
         allowedMethods: AllowedMethods.ALLOW_GET_HEAD,
         cachePolicy: CachePolicy.CACHING_OPTIMIZED,
       },
-      domainNames: ['fringe.jackschaible.ca'],
+      domainNames: ["fringe.jackschaible.ca"],
       certificate: props.certificate,
-      defaultRootObject: 'index.html',
+      defaultRootObject: "index.html",
       errorResponses: [
-        { httpStatus: 403, responsePagePath: '/index.html', responseHttpStatus: 200, ttl: cdk.Duration.seconds(0) },
-        { httpStatus: 404, responsePagePath: '/index.html', responseHttpStatus: 200, ttl: cdk.Duration.seconds(0) },
+        {
+          httpStatus: 403,
+          responsePagePath: "/index.html",
+          responseHttpStatus: 200,
+          ttl: cdk.Duration.seconds(0),
+        },
+        {
+          httpStatus: 404,
+          responsePagePath: "/index.html",
+          responseHttpStatus: 200,
+          ttl: cdk.Duration.seconds(0),
+        },
       ],
     });
 
-    new BucketDeployment(this, 'Deploy', {
-      sources: [Source.asset('../fringe-client/dist/client-new/browser')],
+    new BucketDeployment(this, "Deploy", {
+      sources: [Source.asset("../fringe-client/dist/client-new/browser")],
       destinationBucket: bucket,
       distribution,
-      distributionPaths: ['/*'],
+      distributionPaths: ["/*"],
     });
 
     this.distributionDomain = distribution.distributionDomainName;
